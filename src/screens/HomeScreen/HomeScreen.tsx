ff3db416f5b0a5e0fc85bf4d56f8b29f728ba3c9
gyan gupta <gyanig72@gmail.com>
2020-05-25 18:16:51 +0530

    Added styled input and button component

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { withRouter } from "react-router-dom"; 
+import { Input, Button } from "../../sharedComponents";
 
 interface IProps {}
 
@@ -7,7 +8,16 @@ type Props = IProps;
 
 const HomeScreen: React.FC<Props> = (props: Props) => {
     return (
-        <div>home screen</div>
+        <div>
+            <p>Hi there! Welcome to your education showcase</p>
+            <div>
+                <p>Type you name and click "Enter" below to begin</p>
+                <Input />
+                <Button>
+                    Enter
+                </Button>
+            </div>
+        </div>
     );
 };
 
